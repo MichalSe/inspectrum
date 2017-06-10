7bcc53c55ec19e785e75c130d5f4a9e3e40e66e4
michalse <micsela1@gmail.com>
2017-06-10 20:57:43 +0300

    Commit before deploy

--- gettingstarted/wsgi.py
+++ gettingstarted/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings.prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inspectrum-server.settings.prod")
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
