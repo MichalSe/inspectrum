9a301eb6665f386f597d69c89de66da08c704f5d
michalse <micsela1@gmail.com>
2017-06-10 21:18:45 +0300

    Commit before deploy

--- gettingstarted/wsgi.py
+++ gettingstarted/wsgi.py
@@ -8,6 +8,17 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+
+import os, sys
+# add the hellodjango project path into the sys.path
+#sys.path.append('<PATH_TO_MY_DJANGO_PROJECT>/hellodjango')
+
+# add the virtualenv site-packages path to the sys.path
+#sys.path.append('<PATH_TO_VIRTUALENV>/Lib/site-packages')
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inspectrum-server.settings.prod")
 
 from django.core.wsgi import get_wsgi_application
