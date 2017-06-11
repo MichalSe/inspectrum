60ce2d2933c3ccac056854b494f5fe8e65b060f4
michalse <micsela1@gmail.com>
2017-06-11 12:23:04 +0300

    Commit before deploy

--- gettingstarted/wsgi.py
+++ gettingstarted/wsgi.py
@@ -12,7 +12,7 @@ import os, sys
 # add the hellodjango project path into the sys.path
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inspectrum_server.settings.prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.config.prod")
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
