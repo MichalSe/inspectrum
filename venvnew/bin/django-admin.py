a78baed2173e4fe066716bd6e57f20b46c847334
michalse <micsela1@gmail.com>
2017-06-15 18:48:09 +0300

    requirements update

---
+++ venvnew/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/michalsela/PycharmProjects/inspectrumserverside/venvnew/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
